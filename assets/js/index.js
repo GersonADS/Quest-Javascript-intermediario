df6de7ffe6a14aabfe093dcb41be407a13ac6e3d
GersonADS <shiranuime@gmail.com>
2023-05-07 01:24:54 -0300

    almost

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const $Button = document.querySelector('.btn');
+$Button.addEventListener('click',()=>{
+
+    let $CheckInput = document.querySelectorAll('.check-input');
+
+
+    // for(let i =0;i<=3 ; i++){
+    //     let input = document.querySelectorAll('.check-input')[i]
+    //     console.log(input)
+    // }
+
+});
